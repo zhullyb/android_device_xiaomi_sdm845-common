a2e56623a10e1ae8a5d2b8dbc40c9813efb03729
Kyuofox <88403766@qq.com>
2022-05-08 01:16:43 +0800

    sdm845-common: Switch to xiaomi-sdm845 libperfmgr
    Signed-off-by: Kyuofox <88403766@qq.com>

--- sdm845.mk
+++ sdm845.mk
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-sdm845-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
